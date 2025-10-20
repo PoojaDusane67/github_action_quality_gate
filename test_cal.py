1db4585e22189c93a57c228b668ffb38b6dfe86e
Pooja Dusane <dusanepooja91@gmail.com>
2025-10-21 00:30:08 +0530

    test 2

--- test_cal.py
+++ test_cal.py
@@ -7,11 +7,11 @@ from calc import add, sub, mult, div
 def test_add():
     assert add (2,3)==5
 
-def test_add():
+def test_sub():
     assert sub (6,3)==3
 
-def test_add():
+def test_mult():
     assert mult (2,3)==6
 
-def test_add():
+def test_div():
     assert div (4,2)==2
